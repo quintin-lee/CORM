fded57b6bbb792fbe1497d8069a8c8290f0cf7c5
quintin <2449164582@qq.com>
2026-07-23 14:26:26 +0800

    style(corm_internal): format struct members one per line, fix typo
    - Break corm_registry_t and struct corm into multi-line definitions
    - Fix corm_backet_t typo -> corm_backend_t

--- src/internal/corm_internal.h
+++ src/internal/corm_internal.h
@@ -4,14 +4,24 @@
 #include "corm_pub.h"
 #include "hash.h"
 
-typedef struct{corm_hash_t models_by_table;corm_hash_t models_by_name;}corm_registry_t;
+typedef struct {
+  corm_hash_t models_by_table;
+  corm_hash_t models_by_name;
+} corm_registry_t;
 corm_err_t corm_model_registry_init(corm_registry_t*reg);
 void corm_model_registry_free(corm_registry_t*reg);
 
-struct corm{
-    corm_backend_t*backend;void*conn;corm_config_t config;corm_registry_t registry;
-    corm_err_t last_err;char err_msg[512];char err_sql[1024];
-    int64_t last_insert_id_val;int rows_affected_val;
-    corm_logger_fn logger;void *logger_user_data;
+struct corm {
+  corm_backend_t *backend;
+  void *conn;
+  corm_config_t config;
+  corm_registry_t registry;
+  corm_err_t last_err;
+  char err_msg[512];
+  char err_sql[1024];
+  int64_t last_insert_id_val;
+  int rows_affected_val;
+  corm_logger_fn logger;
+  void *logger_user_data;
 };
 #endif
